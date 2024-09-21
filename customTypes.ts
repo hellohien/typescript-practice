59f48f5aebbd014822cf9a4ad5056ac49ce6a878
Hien Nguyen <hientn179@gmail.com>
2024-09-20 23:26:24 -0700

    Work on custom types

--- customTypes.ts
+++ customTypes.ts
@@ -61,3 +61,37 @@ let ordersArrayTS: [Pet, number][] = [
 
 ordersArrayTS.push(["HAMSTER", 1]); //error: Type '"HAMSTER"' is not assignable to type 'Pet'
 //must use Pet.Hamster instead of HAMSTER
+
+//3. Object Types
+function sayHappyBirthdayWithObject(personObject: {
+  name: string;
+  age: number;
+  giftWish: string;
+  success: boolean;
+}) {
+  let output = "";
+  output += "Happy Birthday " + personObject.name + "! ";
+  output += "You are now " + personObject.age + " years old! ";
+  output +=
+    "Your birthday wish was to receive " +
+    personObject.giftWish +
+    ". And guess what? You will ";
+  if (!personObject.success) {
+    output += "not ";
+  }
+  output += "receive it! \n";
+  console.log(output);
+}
+
+let birthdayBabies: {
+  name: string;
+  age: number;
+  giftWish: string;
+  success: boolean;
+}[] = [
+  { name: "Liam", age: 0, giftWish: "karate skills", success: false },
+  { name: "Olivia", age: 0, giftWish: "a bright future", success: true },
+  { name: "Ava", age: 0, giftWish: "$0.25", success: true },
+];
+
+birthdayBabies.forEach(sayHappyBirthdayWithObject);
