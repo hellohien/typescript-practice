2922f1d2a4e30feb5a6e9582b60283a44090cb00
Hien Nguyen <hientn179@gmail.com>
2024-09-20 23:39:22 -0700

    Work on type aliases

--- customTypes.ts
+++ customTypes.ts
@@ -95,3 +95,9 @@ let birthdayBabies: {
 ];
 
 birthdayBabies.forEach(sayHappyBirthdayWithObject);
+
+//4.Type aliases
+type Coord = [number, number, string, number, number, string];
+
+let codecademyCoordinates: Coord = [40, 43.2, "N", 73, 59.8, "W"];
+let bermudaTCoordinates: Coord = [25, 0, "N", 71, 0, "W"];
