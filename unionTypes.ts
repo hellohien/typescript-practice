a0ce2ec0a5b39ddd26f2bddcdfe2fa58f0d6d604
Hien Nguyen <hientn179@gmail.com>
2024-09-25 10:02:47 -0700

    Work on inferred union return types

--- unionTypes.ts
+++ unionTypes.ts
@@ -19,3 +19,22 @@ function formatValue(value: string | number) {
 
 formatValue("Hiya");
 formatValue(42);
+
+//3. Inferred Union Return Types
+//Declare a variable named userData that stores the returned value of createUser(). Then, go back and provide userData with the correct union type.
+type User = {
+  id: number;
+  username: string;
+};
+
+function createUser() {
+  const randomChance = Math.random() >= 0.5;
+
+  if (randomChance) {
+    return { id: 1, username: "nikko" };
+  } else {
+    return "Could not create a user.";
+  }
+}
+
+let userData: User | string = createUser();
