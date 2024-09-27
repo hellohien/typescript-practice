163b61a84c166733694f0f1367dd058c3b59557d
Hien Nguyen <hientn179@gmail.com>
2024-09-26 21:24:44 -0700

    Work on type guards

--- unionTypes.ts
+++ unionTypes.ts
@@ -102,3 +102,16 @@ function downloadStatus(status: Status) {
 }
 
 downloadStatus("idle");
+
+//7. Type guards
+function formatStatistic(stat: string | number) {
+  if (typeof stat === "number") {
+    return stat.toFixed(2);
+  }
+  if (typeof stat === "string") {
+    return stat.toUpperCase();
+  }
+}
+
+console.log(formatStatistic("Win"));
+console.log(formatStatistic(0.364));
