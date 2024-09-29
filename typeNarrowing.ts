5c7114a0ee6b6cadd3ee2e04cd38104eb8a3d929
Hien Nguyen <hientn179@gmail.com>
2024-09-28 22:14:32 -0700

    Work on narrowing after a type guard

--- typeNarrowing.ts
+++ typeNarrowing.ts
@@ -28,3 +28,31 @@ function prepareEntree(entree: Pasta | Meat) {
 }
 
 console.log(prepareEntree(fettuccine));
+
+//2. Narrowing After a Type Guard
+type Metal = {
+  magnetize: () => string;
+};
+
+type Glass = {
+  melt: () => string;
+};
+
+const iron = {
+  magnetize: () => "Electromagnet activated",
+};
+
+const bottle = {
+  melt: () => "Furnace set to 2,700 degrees",
+};
+
+function recycle(trash: Metal | Glass) {
+  // Add your code below:
+  if ("magnetize" in trash) {
+    return trash.magnetize();
+  }
+
+  return trash.melt();
+}
+
+console.log(recycle(iron));
