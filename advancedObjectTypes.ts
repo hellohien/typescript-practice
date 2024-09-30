acfff33b696592044413ecbaf87b8a1ee5e6765a
Hien Nguyen <hientn179@gmail.com>
2024-09-30 13:26:22 -0700

    Work on index signatures

--- advancedObjectTypes.ts
+++ advancedObjectTypes.ts
@@ -127,3 +127,18 @@ const me: Developer = {
 };
 
 me.code();
+
+//6. Index Signatures
+// Declare an interface named Budget. Use an index signature to make all of its type members have property names of a variable category typed as a string with each key’s value typed as a number
+// Apply the Budget interface to the result variable inside the getBudget() function
+import { getBudgetAsync } from "./api";
+interface Budget {
+  [category: string]: number;
+}
+
+async function getBudget() {
+  const result: Budget = await getBudgetAsync();
+  console.log(result);
+}
+
+getBudget();
