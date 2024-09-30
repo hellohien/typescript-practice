a0a51f0dc444ff13daf524bba78f61a30512ab44
Hien Nguyen <hientn179@gmail.com>
2024-09-29 23:29:48 -0700

    Work on interfaces and classes

--- advancedObjectTypes.ts
+++ advancedObjectTypes.ts
@@ -13,3 +13,27 @@ Percent of goal:  ${(run.miles / 50) * 100}% complete
 updateRunGoal({
   miles: 5,
 });
+
+//2. Interfaces and Classes
+// In the code editor, there’s a program that we’d like to type. The program is the beginning of a computer’s operating system and will allow its users to create directories (aka folders).
+
+// Every directory on the operating system should be able to add a file. Declare an interface named Directory at the top of index.ts. Give it a method name, addFile, whose type is a function with one parameter, name, of type string and returns void.
+interface Directory {
+  addFile: (name: string) => void;
+}
+
+//Using the implements keyword, apply Directory to the DesktopDirectory class.
+class DesktopDirectory implements Directory {
+  addFile(name: string) {
+    console.log(`Adding file: ${name}`);
+  }
+
+  showPreview(name: string) {
+    console.log(`Opening preview of file: ${name}`);
+  }
+}
+
+const Desktop = new DesktopDirectory();
+
+Desktop.addFile("lesson-notes.txt");
+Desktop.showPreview("lesson-notes.txt");
